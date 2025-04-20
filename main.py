1c507301ca79101fe0219c95e0cbd13a734756df
orangci <orangc@proton.me>
2025-04-20 12:38:12 +0300

    meta: add spdx identifiers

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
 import asyncio, os, re, shutil, aiofiles, toml, mistune, pygments, pathlib
 from bs4 import BeautifulSoup
 from datetime import datetime
